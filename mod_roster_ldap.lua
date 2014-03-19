e117da271a6766d0327e87a58ae4cd14dad5dfaa
Franz Pletz <fpletz@fnordicwalking.de>
2014-03-19 17:40:47 +0100

    Retab

--- mod_roster_ldap.lua
+++ mod_roster_ldap.lua
@@ -14,120 +14,120 @@ local group_name = options.group_name or 'Hackerspace';
 local refresh_time = options.refresh_time or 60;
 
 local lc = assert(lualdap.open_simple(ldap_server, ldap_binddn, ldap_bindpass, ldap_tls),
-	'Could not connect to LDAP server');
+                  'Could not connect to LDAP server');
 
 local ldap_roster = {}
 
 local function split(st, sep)
-	local i = 0
-	return function()
-		if i < string.len(st) then
-			local s, e = string.find(st, sep, i)
-			s, e = s or 0, e or string.len(st)
-			local r = string.sub(st, i, s - 1)
-			i = e + 1
-			return r
-		end
-	end
+  local i = 0
+  return function()
+    if i < string.len(st) then
+      local s, e = string.find(st, sep, i)
+      s, e = s or 0, e or string.len(st)
+      local r = string.sub(st, i, s - 1)
+      i = e + 1
+      return r
+    end
+  end
 end
 
 local function dc_to_host(dn)
-	local host, comps = '', {};
-	for comp in split(dn, ',') do
-		table.insert(comps, comp);
-	end
-	for i = #comps, 1, -1 do
-		local comp = comps[i];
-		if not string.match(comp, 'dc=') then
-			return string.sub(host, 1, -2);
-		end
-		host = string.sub(comp, string.find(comp, '=') + 1)..'.'..host;
-	end
+  local host, comps = '', {};
+  for comp in split(dn, ',') do
+    table.insert(comps, comp);
+  end
+  for i = #comps, 1, -1 do
+    local comp = comps[i];
+    if not string.match(comp, 'dc=') then
+      return string.sub(host, 1, -2);
+    end
+    host = string.sub(comp, string.find(comp, '=') + 1)..'.'..host;
+  end
 end
 
 local function ldap_to_entry(dn, attrs)
-	local host = dc_to_host(dn);
-	local na = attrs[ldap_nameattr];
-	local name = type(na) == 'table' and na[1] or na;
-	return { jid = attrs[ldap_uidattr]..'@'..host, name=name };
+  local host = dc_to_host(dn);
+  local na = attrs[ldap_nameattr];
+  local name = type(na) == 'table' and na[1] or na;
+  return { jid = attrs[ldap_uidattr]..'@'..host, name=name };
 end
 
 local function inject_roster_contacts(username, host, roster)
-	for jid, entry in pairs(ldap_roster) do
-		if not (jid == username..'@'..host or roster[jid]) then
-			module:log('debug', 'injecting '..jid..' as '..entry.name)
-			roster[jid] = {};
-			local r = roster[jid];
-			r.subscription = 'both';
-			r.name = entry.name;
-			r.groups = { [group_name] = true };
-			r.persist = false;
-		end
-	end
+  for jid, entry in pairs(ldap_roster) do
+    if not (jid == username..'@'..host or roster[jid]) then
+      module:log('debug', 'injecting '..jid..' as '..entry.name)
+      roster[jid] = {};
+      local r = roster[jid];
+      r.subscription = 'both';
+      r.name = entry.name;
+      r.groups = { [group_name] = true };
+      r.persist = false;
+    end
+  end
 
-	if roster[false] then
-		roster[false].version = true;
-	end
+  if roster[false] then
+    roster[false].version = true;
+  end
 end
 
 
 local function push_all_rosters()
-	for host in pairs(hosts) do
-		for username, session in pairs(hosts[host].sessions) do
-			local roster, new_roster = session.roster, {}
-			for jid, entry in pairs(roster) do
-				if entry.persist ~= false then
-					new_roster[jid] = entry
-				end
-			end
-			inject_roster_contacts(username, host, new_roster)
-		end
-	end
+  for host in pairs(hosts) do
+    for username, session in pairs(hosts[host].sessions) do
+      local roster, new_roster = session.roster, {}
+      for jid, entry in pairs(roster) do
+        if entry.persist ~= false then
+          new_roster[jid] = entry
+        end
+      end
+      inject_roster_contacts(username, host, new_roster)
+    end
+  end
 end
 
 local function update_roster()
-	local iter, err = lc:search { 
-		base = ldap_base;
-		scope = ldap_scope;
-		filter = ldap_filter;
-	}
-	if not iter then
-		print('error', err);
-	end
-	for dn, attrs in iter do
-		local entry = ldap_to_entry(dn, attrs);
-		ldap_roster[entry.jid] = entry
-	end
-	module:log('info', 'updated LDAP roster')
-	--push_all_rosters()
-	--module:log('info', 'pushed updated rosters')
-	return refresh_time
+  local iter, err = lc:search { 
+    base = ldap_base;
+    scope = ldap_scope;
+    filter = ldap_filter;
+  }
+  if not iter then
+    print('error', err);
+  end
+  for dn, attrs in iter do
+    local entry = ldap_to_entry(dn, attrs);
+    ldap_roster[entry.jid] = entry
+  end
+  module:log('info', 'updated LDAP roster')
+  --push_all_rosters()
+  --module:log('info', 'pushed updated rosters')
+  return refresh_time
 end
 
 local function remove_virtual_contacts(username, host, datastore, data)
-	if datastore == 'roster' then
-		local new_roster =  {};
-		for jid, contact in pairs(data) do
-			if contact.persist ~= false then
-				new_roster[jid] = contact
-			end
-		end
-		if new_roster[false] then
-			new_roster[false].version = nil;
-		end
-		return username, host, datastore, new_roster;
-	end
+  if datastore == 'roster' then
+    local new_roster =  {};
+    for jid, contact in pairs(data) do
+      if contact.persist ~= false then
+        new_roster[jid] = contact
+      end
+    end
+    if new_roster[false] then
+      new_roster[false].version = nil;
+    end
+    return username, host, datastore, new_roster;
+  end
 
-	return username, host, datastore, data
+  return username, host, datastore, data
 end
 
 function module.load()
-	update_roster()
-	timer.add_task(refresh_time, update_roster)
-	module:hook('roster-load', inject_roster_contacts);
-	datamanager.add_callback(remove_virtual_contacts);
+  update_roster()
+  timer.add_task(refresh_time, update_roster)
+  module:hook('roster-load', inject_roster_contacts);
+  datamanager.add_callback(remove_virtual_contacts);
 end
 
 function module.unload()
-	datamanager.remove_callback(remove_virtual_contacts);
+  datamanager.remove_callback(remove_virtual_contacts);
 end
