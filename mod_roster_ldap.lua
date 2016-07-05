6c363f9303509e2192b5fb2361f5ea9c540c6079
Franz Pletz <fpletz@fnordicwalking.de>
2016-07-05 22:47:56 +0200

    Add compatibility for ldap2 auth and IPA

--- mod_roster_ldap.lua
+++ mod_roster_ldap.lua
@@ -1,21 +1,11 @@
-local lualdap = require 'lualdap';
+local ldap  = module:require 'ldap';
 local timer = require 'util.timer';
-local options = module:get_option('ldap_roster') or {}
-local ldap_server = options.ldap_server or 'localhost'
-local ldap_tls = options.ldap_tls or false;
-local ldap_base = options.ldap_base or 'ou=people,dc=localhost,dc=localdomain';
-local ldap_group_filter = options.ldap_group_filter or 'cn=xmpp-users';
-local ldap_group_base = options.ldap_group_base or 'ou=group,dc=localhost,dc=localdomain';
-local ldap_binddn = options.ldap_binddn or 'cn=admin,dc=localhost,dc=localdomain';
-local ldap_bindpass = options.ldap_bindpass or 'password';
-local ldap_scope = options.ldap_scope or 'onelevel';
-local ldap_uidattr = options.ldap_uidattr or 'uid';
-local ldap_nameattr = options.ldap_nameattr or 'cn';
-local group_name = options.group_name or 'Members';
-local refresh_time = options.refresh_time or 60;
 
-local lc = assert(lualdap.open_simple(ldap_server, ldap_binddn, ldap_bindpass, ldap_tls),
-                  'Could not connect to LDAP server');
+if not ldap then
+    return;
+end
+
+local params = module:get_option('ldap');
 
 local ldap_roster = {}
 
@@ -48,9 +38,8 @@ end
 
 local function ldap_to_entry(dn, attrs)
   local host = dc_to_host(dn);
-  local na = attrs[ldap_nameattr];
-  local name = type(na) == 'table' and na[1] or na;
-  return { jid = attrs[ldap_uidattr]..'@'..host, name=name };
+  local name = attrs[params.roster.namefield];
+  return { jid = attrs[params.roster.usernamefield]..'@'..host, name=name };
 end
 
 local function inject_roster_contacts(username, host, roster)
@@ -61,7 +50,7 @@ local function inject_roster_contacts(username, host, roster)
       local r = roster[jid];
       r.subscription = 'both';
       r.name = entry.name;
-      r.groups = { [group_name] = true };
+      r.groups = { [params.roster.group_name] = true };
       r.persist = false;
     end
   end
@@ -71,57 +60,25 @@ local function inject_roster_contacts(username, host, roster)
   end
 end
 
-
-local function push_all_rosters()
-  for host in pairs(hosts) do
-    for username, session in pairs(hosts[host].sessions) do
-      local roster, new_roster = session.roster, {}
-      for jid, entry in pairs(roster) do
-        if entry.persist ~= false then
-          new_roster[jid] = entry
-        end
-      end
-      inject_roster_contacts(username, host, new_roster)
-    end
-  end
-end
-
 local function update_roster()
-  local iter, err = lc:search {
-    base = ldap_group_base;
-    scope = ldap_scope;
-    filter = ldap_group_filter;
-    attrs = { 'member' };
-  }
-  if not iter then
-    print('error', err);
-  end
-  for dn, attrs in iter do
-    module:log('debug', 'group: '..tostring(dn))
-    for k, member_dn in pairs(attrs.member) do
-      module:log('debug', ' member: '..tostring(member_dn))
-      local user_filter = string.match(member_dn, "([^,]*)")
-      local iter_members = lc:search {
-        base = ldap_base;
-        scope = ldap_scope;
-        filter = user_filter;
-        attrs = { ldap_nameattr; ldap_uidattr };
-      }
+  local ld = ldap.getconnection();
 
-      if not iter then
-        print('error', err);
-      end
+  local basedn = params.roster.basedn;
+  local filter = params.roster.filter;
 
-      for user_dn, user_attrs in iter_members do
-        local entry = ldap_to_entry(user_dn, user_attrs);
-        ldap_roster[entry.jid] = entry;
-      end
-    end
+  local new_ldap_roster = {};
+
+  for user_dn, user_attrs in ld:search { base = basedn, scope = 'onelevel', filter = filter } do
+    local entry = ldap_to_entry(user_dn, user_attrs);
+    new_ldap_roster[entry.jid] = entry;
   end
-  module:log('info', 'updated LDAP roster')
-  --push_all_rosters()
-  --module:log('info', 'pushed updated rosters')
-  return refresh_time
+
+  ldap_roster = new_ldap_roster;
+  module:log('info', 'updated LDAP roster');
+
+  ld:close();
+
+  return params.roster.refresh_time
 end
 
 local function remove_virtual_contacts(username, host, datastore, data)
@@ -143,7 +100,7 @@ end
 
 function module.load()
   update_roster()
-  timer.add_task(refresh_time, update_roster)
+  timer.add_task(params.roster.refresh_time, update_roster)
   module:hook('roster-load', inject_roster_contacts);
   datamanager.add_callback(remove_virtual_contacts);
 end
